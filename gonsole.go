108a77cf8755b771bf21ef74279ed15ebb79468b
Wall Tearer <walltearer@gmail.com>
2016-05-27 17:49:36 +0200

    Initial commit

---
+++ gonsole.go
@@ -0,0 +1,47 @@
+package gonsole
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Gonsole struct {
+	reader *bufio.Reader
+}
+
+func New() *Gonsole {
+	gon := &Gonsole{}
+	gon.reader = bufio.NewReader(os.Stdin)
+	return gon
+}
+
+// Asks user to enter a valid integer value until he enters correct value.
+func (gon *Gonsole) ReadInt(msg string) int {
+	var readSuccess bool = false
+	var inputInt int
+	var errConv error
+
+	for readSuccess == false {
+		fmt.Printf("%s", msg)
+
+		inputStr, err := gon.reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("Reading Error: %v\n", err)
+			continue
+		}
+
+		inputStr = strings.TrimSpace(inputStr)
+		inputInt, errConv = strconv.Atoi(inputStr)
+		if errConv != nil {
+			fmt.Printf("Converting Error: %v\n", errConv)
+			continue
+		}
+
+		readSuccess = true
+	}
+
+	return inputInt
+}
